7b7a710110366991162a77c918c6e2fdff26bf35
Yuval Marcus <yuval.marcus.90@gmail.com>
2024-11-24 19:32:39 +0200

    FE: remove unnecessary code in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Home from "./feature/Home/Home.tsx";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import Layout from "./components/Layout/Layout.tsx";
