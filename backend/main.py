09bdce1533ea725b42484fc217113cea1350d055
Yarin Levi <yarinl330@gmail.com>
2024-12-01 01:11:58 +0200

    backend: Changing database from sqlite to postgres.

--- backend/main.py
+++ backend/main.py
@@ -99,7 +99,7 @@ def register(user: User):
     if is_valid_username(user.username) and is_valid_password(user.password) and is_valid_email(user.email):
         tags_id = get_tags_id(user.tags)
         insert_user_to_db(username=user.username.lower(), hashed_password=get_password_hash(user.password),
-                          email=user.email, is_blocked=0, is_deleted=0, profile_photo=None, cover_photo=None,
+                          email=user.email, is_blocked=False, is_deleted=False, profile_photo=None, cover_photo=None,
                           about=None, disabled=True)
         insert_favorite_tags_to_db(user.username, tags_id)
         connect_user(user.username)
