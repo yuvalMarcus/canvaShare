f405a4d23a980eb950c895e236f36e8a4db792b0
Yuval Marcus <yuval.marcus.90@gmail.com>
2024-12-06 16:08:00 +0200

    fix error map - key in Shape.tsx file

--- frontend/src/feature/Painter/Toolkit/Shape/Shape.tsx
+++ frontend/src/feature/Painter/Toolkit/Shape/Shape.tsx
@@ -1,7 +1,7 @@
 import {Box, Slider, Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {grey} from "@mui/material/colors";
-import React, {FC, MutableRefObject, useState} from "react";
+import React, {FC, Fragment, MutableRefObject, useState} from "react";
 import Button from "@mui/material/Button";
 import {v4 as uuidv4} from "uuid";
 import {Canvas} from "fabric";
@@ -49,7 +49,11 @@ const Shape: FC<ShapeProps> = ({ canvas, onClose }) => {
             <Box>
                 <Typography color={grey[100]} fontSize={18} mb={1}>Shapes: </Typography>
                 <Stack flexDirection="row" justifyContent="space-between">
-                    {Object.values(SHAPE_TYPE).map((shape: SHAPE_TYPE) => mapShapeToIcon[shape](selectedShape === shape, () => setSelectedShape(shape)))}
+                    {Object.values(SHAPE_TYPE).map((shape: SHAPE_TYPE) => (
+                        <Fragment key={shape}>
+                            {mapShapeToIcon[shape](selectedShape === shape, () => setSelectedShape(shape))}
+                        </Fragment>
+                    ))}
                 </Stack>
             </Box>
             <Box>
