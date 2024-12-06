9306b2ff94a3ffa954e1881713792ff6e0c641dc
Yuval Marcus <yuval.marcus.90@gmail.com>
2024-12-06 15:16:10 +0200

    update Register code

--- frontend/src/feature/Register/Register.tsx
+++ frontend/src/feature/Register/Register.tsx
@@ -4,13 +4,15 @@ import {Box, CircularProgress, Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {green, grey, red} from "@mui/material/colors";
+import {grey, red} from "@mui/material/colors";
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
 import * as api from "../../api/auth.ts";
 import {useMutation} from "@tanstack/react-query";
 import {RegisterPayload} from "../../types/auth.ts";
 import {FC} from "react";
 import InputText from "../../components/Form/InputText/InputText.tsx";
+import {Bounce, toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 const schema = z.object({
     username: z.string().min(4, { message: 'Required' }),
@@ -29,8 +31,41 @@ const Register: FC = () => {
         resolver: zodResolver(schema),
     });
 
+    const navigate = useNavigate();
+
+    const handleOnSuccess = () => {
+        toast.success('created successfully', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+        });
+        navigate("/login");
+    }
+
+    const handleOnError = () => {
+        toast.error('error', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+        });
+    }
+
     const { mutateAsync, isSuccess, isPending, isError, isIdle } = useMutation({
-        mutationFn: api.register
+        mutationFn: api.register,
+        onSuccess: handleOnSuccess,
+        onError: handleOnError
     })
 
     const onSubmit = async ({ username, email, password, tags }: RegisterPayload) => {
@@ -49,38 +84,36 @@ const Register: FC = () => {
                     <AppRegistrationIcon fontSize="large" />
                     <Typography variant="h4" textAlign="center" color={grey[700]}>Register</Typography>
                 </Stack>
-                {isIdle && (
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Box>
-                            <InputText label="username" name="username" control={control} />
-                            <Typography color={red[700]} height={30}>{errors.username?.message}</Typography>
-                        </Box>
-                        <Box>
-                            <InputText label="email" name="email" control={control} />
-                            <Typography color={red[700]} height={30}>{errors.email?.message}</Typography>
-                        </Box>
-                        <Box>
-                            <InputText label="password" name="password" control={control} />
-                            <Typography color={red[700]} height={30}>{errors.password?.message}</Typography>
-                        </Box>
-                        <Box>
-                            <InputText label="confirm password" name="confirmPassword" control={control} />
-                            <Typography color={red[700]} height={30}>{errors.confirmPassword?.message}</Typography>
-                        </Box>
-                        <Button variant="outlined" fullWidth type="submit">submit</Button>
-                    </form>
-                )}
-                {isPending && (
-                    <Box textAlign="center">
-                        <CircularProgress />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Box>
+                        <InputText label="username" name="username" control={control} />
+                        <Typography color={red[700]} height={30}>{errors.username?.message}</Typography>
                     </Box>
-                )}
-                {isSuccess && (
-                    <Typography color={green[700]} textTransform="capitalize">user created successfully</Typography>
-                )}
-                {isError && (
-                    <Typography color={red[700]} textTransform="capitalize">error</Typography>
-                )}
+                    <Box>
+                        <InputText label="email" name="email" control={control} />
+                        <Typography color={red[700]} height={30}>{errors.email?.message}</Typography>
+                    </Box>
+                    <Box>
+                        <InputText label="password" name="password" control={control} />
+                        <Typography color={red[700]} height={30}>{errors.password?.message}</Typography>
+                    </Box>
+                    <Box>
+                        <InputText label="confirm password" name="confirmPassword" control={control} />
+                        <Typography color={red[700]} height={30}>{errors.confirmPassword?.message}</Typography>
+                    </Box>
+                    <Button variant="outlined" fullWidth type="submit" disabled={isPending}>
+                        {isPending && (
+                            <Stack alignItems="center" justifyContent="center">
+                                <CircularProgress size={24} />
+                            </Stack>
+                        )}
+                        {!isPending && (
+                            <Typography textAlign="center">
+                                submit
+                            </Typography>
+                        )}
+                    </Button>
+                </form>
             </Box>
         </Stack>
     )
