7f90c3b1909087efb2c825a534cc8349253f33b0
Yuval Marcus <yuval.marcus.90@gmail.com>
2024-12-06 15:15:48 +0200

    update Login code

--- frontend/src/feature/Login/Login.tsx
+++ frontend/src/feature/Login/Login.tsx
@@ -4,7 +4,7 @@ import {Box, CircularProgress, Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {blueGrey, green, grey, red} from "@mui/material/colors";
+import {blueGrey, grey, red} from "@mui/material/colors";
 import LoginIcon from '@mui/icons-material/Login';
 import * as api from '../../api/auth.ts';
 import {LoginPayload} from "../../types/auth.ts";
@@ -12,6 +12,8 @@ import InputText from "../../components/Form/InputText/InputText.tsx";
 import {useMutation} from "@tanstack/react-query";
 import {useAuth} from "../../hooks/useAuth.ts";
 import {FC} from "react";
+import {Bounce, toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 const schema = z.object({
     username: z.string().min(4, { message: 'required' }),
@@ -22,6 +24,8 @@ const Login: FC = () => {
 
     const { login } = useAuth();
 
+    const navigate = useNavigate();
+
     const {
         control,
         handleSubmit,
@@ -30,8 +34,39 @@ const Login: FC = () => {
         resolver: zodResolver(schema),
     });
 
+    const handleOnSuccess = () => {
+        toast.success('login successfully', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+        });
+        navigate("/");
+    }
+
+    const handleOnError = () => {
+        toast.error('error', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+        });
+    }
+
     const { mutateAsync, isSuccess, isPending, isError, isPaused, isIdle } = useMutation({
-        mutationFn: api.login
+        mutationFn: api.login,
+        onSuccess: handleOnSuccess,
+        onError: handleOnError
     })
 
     const onSubmit = async ({ username, password }: LoginPayload) => {
@@ -50,30 +85,28 @@ const Login: FC = () => {
                     <LoginIcon fontSize="large" />
                     <Typography variant="h4" textAlign="center" color={blueGrey.A700}>Login</Typography>
                 </Stack>
-                {isIdle && (
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Box>
-                            <InputText label="username" name="username" control={control} />
-                            <Typography color={red[700]} height={30}>{errors.username?.message}</Typography>
-                        </Box>
-                        <Box>
-                            <InputText label="password" name="password" control={control} />
-                            <Typography color={red[700]} height={30}>{errors.password?.message}</Typography>
-                        </Box>
-                        <Button variant="outlined" fullWidth type="submit">submit</Button>
-                    </form>
-                )}
-                {isPending && (
-                    <Box textAlign="center">
-                        <CircularProgress />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Box>
+                        <InputText label="username" name="username" control={control} />
+                        <Typography color={red[700]} height={30}>{errors.username?.message}</Typography>
                     </Box>
-                )}
-                {isSuccess && (
-                    <Typography color={green[700]} textTransform="capitalize">user login successfully</Typography>
-                )}
-                {isError && (
-                    <Typography color={red[700]} textTransform="capitalize">error</Typography>
-                )}
+                    <Box>
+                        <InputText label="password" name="password" control={control} />
+                        <Typography color={red[700]} height={30}>{errors.password?.message}</Typography>
+                    </Box>
+                    <Button variant="outlined" fullWidth type="submit" disabled={isPending}>
+                        {isPending && (
+                            <Stack alignItems="center" justifyContent="center">
+                                <CircularProgress size={24} />
+                            </Stack>
+                        )}
+                        {!isPending && (
+                            <Typography textAlign="center">
+                                submit
+                            </Typography>
+                        )}
+                    </Button>
+                </form>
             </Box>
         </Stack>
     )
