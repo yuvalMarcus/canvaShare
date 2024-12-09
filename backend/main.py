e983293b8d782aa5174558aebeb8e5ffdbfe6d2d
Yarin Levi <yarinl330@gmail.com>
2024-12-09 16:53:53 +0200

    backend: removed useless lines of code

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
-from canvas import router as canvas_router
+from user import user_prefixed_router, router as user_router
 from fastapi.middleware.cors import CORSMiddleware
-from user import user_prefixed_router,router as user_router
 from report import router as report_router
+from canvas import router as canvas_router
 from photo import router as photo_router
 from tag import router as tag_router
 from fastapi import FastAPI
@@ -11,19 +11,12 @@ import logging
 import uvicorn
 
 load_dotenv()
-FRONT_DOMAIN, FRONT_PORT = os.getenv('FRONT_DOMAIN'), os.getenv('FRONT_PORT')
-BACK_DOMAIN, BACK_PORT = os.getenv('BACK_DOMAIN'), int(os.getenv('BACK_PORT'))
-
 app = FastAPI()
 for router in [canvas_router, user_router, report_router, photo_router, tag_router, user_prefixed_router]:
     app.include_router(router)
 
-origins = [
-    f"{FRONT_DOMAIN}:{FRONT_PORT}"
-]
-
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-
+app.add_middleware(CORSMiddleware, allow_origins=[os.getenv('ORIGIN')], allow_credentials=True, allow_methods=["*"],
+                   allow_headers=["*"])
 
 delete_tables_and_folders()
 create_tables_and_folders()
@@ -31,4 +24,4 @@ create_tables_and_folders()
 if __name__ == "__main__":
     logger = logging.getLogger()
     logger.setLevel(logging.ERROR)
-    uvicorn.run(app, host=BACK_DOMAIN, port=BACK_PORT)
+    uvicorn.run(app, host=os.getenv('BACK_DOMAIN'), port=int(os.getenv('BACK_PORT')))
