f8b51461deb4d4515c0c3d631ea37ada326aa2e8
Yarin Levi <yarinl330@gmail.com>
2024-12-20 14:58:46 +0200

    backend: change get likes with filters

--- backend/like.py
+++ backend/like.py
@@ -1,31 +1,27 @@
 from fastapi import APIRouter
-from classes import Like, Likes
+from classes import Like, Likes, LikesNumber
 from db_utils import *
 from auth import *
 
 router = APIRouter(prefix="/like")
 
-@router.post('', response_model=Like)
-def like(like_obj: Like, jwt_user_id: int = Depends(check_guest_or_blocked)) -> Like:
+@router.post('', response_model=LikesNumber)
+def like(like_obj: Like, jwt_user_id: int = Depends(check_guest_or_blocked)) -> LikesNumber:
     canvas_id = like_obj.canvas_id
     raise_error_if_blocked(get_canvas_user_id(canvas_id)) # Cannot like a blocked creator's canvas.
     like_or_unlike_canvas(canvas_id, jwt_user_id, like_flag=True)
     return {"canvas_id": canvas_id, "likes": get_num_of_likes(canvas_id)}
 
-@router.delete('', response_model=Like)
-def unlike(like_obj: Like, jwt_user_id: int = Depends(check_guest_or_blocked)) -> Like:
+@router.delete('', response_model=LikesNumber)
+def unlike(like_obj: Like, jwt_user_id: int = Depends(check_guest_or_blocked)) -> LikesNumber:
     canvas_id = like_obj.canvas_id
     raise_error_if_blocked(get_canvas_user_id(canvas_id)) # Cannot unlike a blocked creator's canvas.
     like_or_unlike_canvas(canvas_id, jwt_user_id, like_flag=False)
     return {"canvas_id": canvas_id, "likes": get_num_of_likes(canvas_id)}
 
 @router.get('', response_model=Likes)
-def get_canvases_likes_number(canvas_id: Optional[int] = None) -> Likes:
-    if canvas_id:
-        get_canvas_from_db(canvas_id)  # checks if canvas exist
-        return {"results": [{"canvas_id": canvas_id, "likes": get_num_of_likes(canvas_id)}]}
-    else:
-        results = []
-        for canvas in get_canvases_likes():
-            results.append({"canvas_id": canvas[0], "likes": canvas[1]})
-        return {"results": results}
+def get_canvases_likes_number(canvas_id: Optional[int] = None, user_id: Optional[int] = None) -> Likes:
+    results = []
+    for canvas in get_canvases_likes(canvas_id, user_id):
+        results.append({"canvas_id": canvas[0], "user_id": canvas[1]})
+    return {"results": results}
