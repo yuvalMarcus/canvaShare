b319216e0635dcf9ffcd458d2b982c1b47482a99
Yuval Marcus <yuval.marcus.90@gmail.com>
2024-12-21 17:32:14 +0200

    update painter state

--- frontend/src/api/painter.ts
+++ frontend/src/api/painter.ts
@@ -10,6 +10,23 @@ export const searchPhotos = async (params: Record<string, string>): Promise<axio
     return data;
 }
 
+export const getPainter = async (id: number): Promise<axios.AxiosResponse> => {
+    const res = await instance.get(`canvas/${id}`);
+    return res.data;
+}
+
+export const getPainters = async (params): Promise<axios.AxiosResponse> => {
+    const res = await instance.get('canvas', {
+        params
+    });
+    return res.data;
+}
+
 export const createPainter = async (payload: CanvasPayload): Promise<axios.AxiosResponse> => {
     return await instance.post('canvas', payload);
+}
+
+
+export const deletePainter = async (id: number): Promise<axios.AxiosResponse> => {
+     await instance.delete(`canvas/${id}`);
 }
