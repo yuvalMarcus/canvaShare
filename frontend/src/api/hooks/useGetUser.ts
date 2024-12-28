b9048f1a4e69eff2d9239e5032d66e93d94ee6be
Yuval Marcus <yuval.marcus.90@gmail.com>
2024-12-28 17:32:16 +0200

    update api hooks

---
+++ frontend/src/api/hooks/useGetUser.ts
@@ -0,0 +1,21 @@
+import {useQuery} from "@tanstack/react-query";
+import * as userApi from "../user.ts";
+
+export const GET_USER = 'getUser';
+
+const useGetUser = (id?: string | number) => {
+
+    const { data, isPending, isError } = useQuery({
+        queryKey: [GET_USER, id],
+        queryFn: () => userApi.getUser(id),
+        enabled: !!id
+    });
+
+    return {
+        data,
+        isPending,
+        isError
+    }
+}
+
+export default useGetUser;
