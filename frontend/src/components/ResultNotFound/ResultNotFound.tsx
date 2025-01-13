a0fcd3d6762b59b08afa63a223f6930d56fcb433
Yahel Megidish <megidishyahel@gmail.com>
2025-01-13 23:31:44 +0200

    changed icon size

--- frontend/src/components/ResultNotFound/ResultNotFound.tsx
+++ frontend/src/components/ResultNotFound/ResultNotFound.tsx
@@ -5,7 +5,7 @@ const ResultNotFound = () => {
 
     return (
         <Stack justifyContent="center" alignItems="center" gap={2}>
-            <img src="/assets/no-results.png" alt="no result" style={{width: '5%'}}/>
+            <img src="/assets/no-results.png" alt="no result" style={{width: '20%'}}/>
             <Typography variant="h4">
                 Result not found!</Typography>
             <Typography variant="h6">
