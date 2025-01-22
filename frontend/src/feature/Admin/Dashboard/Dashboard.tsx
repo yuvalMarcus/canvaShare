819bef89921c8565da5925c1549c60fa450c9bb9
Yuval Marcus <yuval.marcus.90@gmail.com>
2025-01-22 21:39:57 +0200

    update admin dashboard

--- frontend/src/feature/Admin/Dashboard/Dashboard.tsx
+++ frontend/src/feature/Admin/Dashboard/Dashboard.tsx
@@ -1,55 +1,73 @@
-import {CardContent, Stack} from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Card from "@mui/material/Card";
+import {Box, Stack} from "@mui/material";
 import Button from "@mui/material/Button";
 import GroupIcon from '@mui/icons-material/Group';
 import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual';
+import DashboardCard from "./DashboardCard/DashboardCard.tsx";
+import TagIcon from "@mui/icons-material/Tag";
+import useGetUsers from "../../../api/hooks/user/useGetUsers.ts";
+import useGetPaints from "../../../api/hooks/paint/useGetPaints.ts";
+import useGetTags from "../../../api/hooks/tag/useGetTags.ts";
+import useGetReports from "../../../api/hooks/report/useGetReports.ts";
+import Permissions from "../../../components/Permissions/Permissions.tsx";
+import {useNavigate} from "react-router-dom";
 
 const Dashboard = () => {
+
+    const navigate = useNavigate();
+
+    const { data: users, isPending } = useGetUsers({});
+
+    const { data: paints } = useGetPaints();
+
+    const { data: reports } = useGetReports();
+
+    const { data: tags } = useGetTags();
+
     return (
-        <Stack flexDirection="row" gap={4}>
-            <Card variant="outlined" sx={{ height: '100%', flexGrow: 1, boxShadow: 2 }}>
-                <CardContent>
-                    <Typography component="h2" variant="subtitle2" gutterBottom>
-                        users
-                    </Typography>
-                    <Stack direction="column" sx={{ justifyContent: 'space-between', flexGrow: '1', gap: 2 }}>
-                        <Stack sx={{ justifyContent: 'space-between' }}>
-                            <Stack direction="row" sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                                <Typography variant="h4" component="p">
-                                    123
-                                </Typography>
-                                <GroupIcon fontSize="large" />
+        <Stack gap={8}>
+            <Stack flexDirection="row" gap={8}>
+                <Permissions roles={['user_view']}>
+                    <Box width="50%">
+                        <DashboardCard title="users" Icon={<GroupIcon fontSize="large" />} value={users?.results?.length}>
+                            <Stack flexDirection="row" gap={2}>
+                                <Button variant="contained" onClick={() => navigate('/admin/users')}>view users</Button>
+                                <Permissions roles={['user_management']}>
+                                    <Button variant="contained" onClick={() => navigate('/admin/users/create')}>add user</Button>
+                                </Permissions>
                             </Stack>
-                        </Stack>
-                        <Stack flexDirection="row" gap={2}>
-                            <Button variant="contained">view users</Button>
-                            <Button variant="contained">add user</Button>
-                        </Stack>
-                    </Stack>
-                </CardContent>
-            </Card>
-            <Card variant="outlined" sx={{ height: '100%', flexGrow: 1, boxShadow: 2 }}>
-                <CardContent>
-                    <Typography component="h2" variant="subtitle2" gutterBottom>
-                        users
-                    </Typography>
-                    <Stack direction="column" sx={{ justifyContent: 'space-between', flexGrow: '1', gap: 2 }}>
-                        <Stack sx={{ justifyContent: 'space-between' }}>
-                            <Stack direction="row" sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                                <Typography variant="h4" component="p">
-                                    123
-                                </Typography>
-                                <PhotoSizeSelectActualIcon fontSize="large" />
+                        </DashboardCard>
+                    </Box>
+                </Permissions>
+                <Permissions roles={['paint_view']}>
+                    <Box width="50%">
+                        <DashboardCard title="paints" Icon={<PhotoSizeSelectActualIcon fontSize="large" />} value={paints?.results.length}>
+                            <Stack flexDirection="row" gap={2}>
+                                <Button variant="contained" onClick={() => navigate('/admin/paints')}>view paints</Button>
                             </Stack>
-                        </Stack>
-                        <Stack flexDirection="row" gap={2}>
-                            <Button variant="contained">view users</Button>
-                            <Button variant="contained">add user</Button>
-                        </Stack>
-                    </Stack>
-                </CardContent>
-            </Card>
+                        </DashboardCard>
+                    </Box>
+                </Permissions>
+            </Stack>
+            <Stack flexDirection="row" gap={8}>
+                <Permissions roles={['report_view']}>
+                    <Box width="50%">
+                        <DashboardCard title="reports" Icon={<GroupIcon fontSize="large" />} value={tags?.results.length}>
+                            <Stack flexDirection="row" gap={2}>
+                                <Button variant="contained" onClick={() => navigate('/admin/reports')}>view reports</Button>
+                            </Stack>
+                        </DashboardCard>
+                    </Box>
+                </Permissions>
+                <Permissions roles={['tags_view']}>
+                    <Box width="50%">
+                        <DashboardCard title="tags" Icon={<TagIcon fontSize="large" />} value={tags?.results.length}>
+                            <Stack flexDirection="row" gap={2}>
+                                <Button variant="contained" onClick={() => navigate('/admin/tags')}>view tags</Button>
+                            </Stack>
+                        </DashboardCard>
+                    </Box>
+                </Permissions>
+            </Stack>
         </Stack>
     )
 }
